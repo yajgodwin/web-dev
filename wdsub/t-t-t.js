8e27e5795e8ac43411a2e98e46321adf3b2a2d83
yajgodwin <student.jg@youngsvilleacademy.org>
2026-05-22 13:29:50 -0400

    Create t-t-t.js

---
+++ wdsub/t-t-t.js
@@ -0,0 +1,84 @@
+let gameOverDiv = document.getElementById("game-over");
+let winner = document.querySelector("#game-over p");
+let squareArr = document.querySelectorAll(".ttt-square");
+let currentPlayer = "X";
+
+for (const elem of squareArr) {
+  elem.addEventListener("click", (event) => drawSymbol(event) );
+}
+
+
+function drawSymbol(event){
+  let clickedSquare = event.target;
+  
+ if (clickedSquare.innerText == ""){
+   clickedSquare.innerText = currentPlayer;
+   if (checkForWinner() == false) checkForDraw();
+  changePlayer();
+ }
+}
+
+function checkForWinner(){
+  let isGameOver = false;
+  
+  //check for three in a column
+  for(let i = 0 ; i < 3 ; i++ ){
+  if(squareArr[i].innerText == currentPlayer
+   && squareArr[i + 3].innerText == currentPlayer
+   && squareArr[i + 6].innerText == currentPlayer) isGameOver = true;
+  }
+  
+  //check for three in a row 
+  for(let i = 0 ; i < 3 ; i++ ){
+  if(squareArr[i * 3].innerText == currentPlayer
+   && squareArr[(i * 3) + 1].innerText == currentPlayer
+   && squareArr[(i * 3) + 2 ].innerText == currentPlayer) isGameOver = true;
+  }
+  
+  //check for three in a diagonal 
+ if(squareArr[0].innerText == currentPlayer
+   && squareArr[4].innerText == currentPlayer
+   && squareArr[8].innerText == currentPlayer) isGameOver = true;
+ else  if(squareArr[2].innerText == currentPlayer
+   && squareArr[4].innerText == currentPlayer
+   && squareArr[6].innerText == currentPlayer) isGameOver = true;
+  
+  
+  if (isGameOver) showWinner();
+  return isGameOver;
+}
+
+function showWinner(){
+  gameOverDiv.style.display = "block";
+  winner.innerText = "Player " + currentPlayer + " Won!";
+}
+
+function checkForDraw(){
+  let isDraw = true;
+  
+  for(const elem of squareArr){
+   if (elem.innerText == "") isDraw = false;
+  }
+  
+  if (isDraw) showDraw();
+}
+
+function showDraw(){
+  gameOverDiv.style.display = "block";
+  winner.innerText = " No Player Won !";
+}
+
+function changePlayer(){
+currentPlayer = ((currentPlayer == "X") ? "O" : "X");
+}
+
+function clearBoard(){
+  //hide gameoverdiv
+  gameOverDiv.style.display = "none";
+  
+  //remove the symbols
+  
+  for(const elem of squareArr){
+    elem.innerText  = "";
+  }
+}
